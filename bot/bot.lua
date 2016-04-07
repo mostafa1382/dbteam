f35fa188c6c7f506ce9be9c28eae1390b700a699
mostafa1382 <mostafataghavi1382@gmail.com>
2016-04-07 11:48:17 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -246,7 +246,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {117303449,134708484,193786098},
     admin_users = {},
     disabled_channels = {}
   }
